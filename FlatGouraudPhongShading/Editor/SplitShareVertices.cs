5f90d7833a29724bba0226258712fbf3af9feb51
wpszz <shenzizai@qq.com>
2018-06-11 14:19:33 +0800

    add mesh duplicate tool(split shared vertices)

---
+++ FlatGouraudPhongShading/Editor/SplitShareVertices.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SplitShareVertices : EditorWindow
+{
+    [MenuItem("GameObject/SplitShareVertices", false, 3)]
+    public static void ShowDialog()
+    {
+        MeshFilter mf = Selection.activeGameObject.GetComponent<MeshFilter>();
+        if (mf == null)
+        {
+            Debug.LogWarning("No MeshFilter Component on the activeGameObject");
+            return;
+        }
+
+        string savePath = EditorUtility.SaveFilePanelInProject("Save to", Selection.activeGameObject.name, "asset", "split mesh shared vertices and save to disk.");
+        if (string.IsNullOrEmpty(savePath))
+            return;
+
+        Mesh mesh = Instantiate(mf.sharedMesh) as Mesh;
+
+        int[] triangles = mesh.triangles;
+        Vector3[] originVerts = mesh.vertices;
+        Vector3[] splitVerts = new Vector3[triangles.Length];
+        for (int i = 0; i < triangles.Length; i++)
+        {
+            splitVerts[i] = originVerts[triangles[i]];
+            triangles[i] = i;
+        }
+        mesh.vertices = splitVerts;
+        mesh.triangles = triangles;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        AssetDatabase.CreateAsset(mesh, savePath);
+        AssetDatabase.SaveAssets();
+    }
+}
